8cfc904132a9704e5bcdc4fd559049d004579f27
Alex Morega <alex@grep.ro>
2021-01-21 18:47:08 +0200

    Original requirements

---
+++ requirements/compile_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+pip-compile nlp.in
+pip-compile base.in
+pip-compile dev.in
+pip-compile tests.in
+pip-compile prod.in
+pip-compile docs.in
+pip-compile all.in
